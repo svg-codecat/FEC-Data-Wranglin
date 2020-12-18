8933a976f872c9fe170ba936c043577bddddf1ec
BOsterbuhr <brodyosterbuhr@gmail.com>
2020-12-17 21:36:25 -0600

    More optimization improvements

--- get_that_data.py
+++ get_that_data.py
@@ -1,6 +1,6 @@
 from src.data.data_fetcher import DataFetcher
 
-fetcher = DataFetcher("2010", "S")
+fetcher = DataFetcher("2020", "P")
 fetcher.api_starting_url_container
-fetcher.gimmie_data(record_limit=4)
+fetcher.gimmie_data(record_limit=50000)
 fetcher.save_df_data() 
