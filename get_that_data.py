a8e748aacc730c74a23b69a307f9df721ea4e594
BOsterbuhr <brodyosterbuhr@gmail.com>
2020-12-20 21:44:38 -0600

    Added unit tests and docstrings for clean_data.py

--- get_that_data.py
+++ get_that_data.py
@@ -2,5 +2,5 @@ from src.data.data_fetcher import DataFetcher
 
 fetcher = DataFetcher("2020", "P")
 fetcher.api_starting_url_container
-fetcher.gimmie_data(record_limit=50000)
+fetcher.gimmie_data(record_limit=500)
 fetcher.save_df_data() 
--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -7,7 +7,7 @@ from ftfy import fix_text
 
 # Imports for awesome_cossim_top()
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, isspmatrix_csr
 import sparse_dot_topn.sparse_dot_topn as ct
 
 # Import for vectorize
@@ -26,7 +26,31 @@ def _get_df_columns(df):
 
 
 class DataCleaner:
-    
+    """
+    Instantiated with a dataframe its column names, and size of lowest_similarity and ngram_size. 
+    DataCleaner methods to provide a smart deduplication of column values in a dataframe.
+    Utilizes a form of Term Frequency, Inverse Document Frequency to determine similarity between values.
+
+
+    Parameters:
+        path: Pandas.DataFrame.
+            Use _csv_to_df to provide df from csv.
+        
+        lowest_similarity: float
+            Lowest similarity percentage of column values to combine.
+                i.e. 0.9 = words with 90% or more similarity are combined.
+
+        column_name: str
+            A column name from path. Use _get_df_columns to get a list of columns.
+
+        ngram_size: int
+            Size of string chunks used to assess similarity between two values.
+            3 is normally best but values between 2 and 5 can work.
+
+    Returns:
+        A Pandas DataFrame with similar values of column_name combined.
+
+    """
 
     def __init__(self, path: pd.DataFrame, lowest_similarity: float, column_name: str, ngram_size: int):
         self.df = path
@@ -36,6 +60,17 @@ class DataCleaner:
         
 
     def _ngrams(self, string):
+        """
+        Takes input `string`, removes unwanted chars and returns a list of slices of input `string`.
+
+        Parameters:
+            String: A string to be chunked into size of self.ngram_size.
+
+        Returns:
+            A list of strings. 
+                i.e. testing with self.ngram_size of 3
+                    [' Te', 'Tes', 'est', 'sti', 'tin', 'ing', 'ng ']
+        """
         string = str(string)
         string = fix_text(string) # fix text
         string = string.encode("ascii", errors="ignore").decode() #remove non ascii chars
@@ -55,17 +90,47 @@ class DataCleaner:
 
 
     def _awesome_cossim_top(self, A, B, ntop):
-        # force A and B as a CSR matrix.
-        # If they have already been CSR, there is no overhead
-        A = A.tocsr()
-        B = B.tocsr()
-        M, _ = A.shape
-        _, N = B.shape  
+        """
+        True magic, an improvment on scikit-learn's cosine_similarity function.
+        Thanks to ING BANK.
+
+        ING definition:
+            This function will return a matrxi C in CSR format, where
+            C = [sorted top n results and results > lower_bound for each row of A * B]
+                Input:
+                    A and B: two CSR matrix
+                    ntop: n top results
+                    self.lowest_similarity: a threshold that the element of A*B must greater than
+                Output:
+                    C: result matrix
+            N.B. if A and B are not CSR format, they will be converted to CSR
+
+        """
+        if not isspmatrix_csr(A):
+            A = A.tocsr()
+
+        if not isspmatrix_csr(B):
+            B = B.tocsr()
+
+        M, K1 = A.shape
+        K2, N = B.shape
+
+        if K1 != K2:
+            err_str = 'A matrix multiplication will be operated. A.shape[1] must be equal to B.shape[0]!'
+            raise ValueError(err_str)
  
         idx_dtype = np.int32
  
         nnz_max = M*ntop
- 
+
+        # basic check. if A or B are all zeros matrix, return all zero matrix directly
+        if len(A.indices) == 0 or len(B.indices) == 0:
+            indptr = np.zeros(M + 1, dtype=idx_dtype)
+            indices = np.zeros(nnz_max, dtype=idx_dtype)
+            data = np.zeros(nnz_max, dtype=A.dtype)
+            return csr_matrix((data, indices, indptr), shape=(M, N))
+
+        # filled matrices from here on
         indptr = np.zeros(M+1, dtype=idx_dtype)
         indices = np.zeros(nnz_max, dtype=idx_dtype)
         data = np.zeros(nnz_max, dtype=A.dtype)
@@ -85,6 +150,11 @@ class DataCleaner:
 
 
     def _get_matches_df(self, sparse_matrix, name_vector):
+        """
+        Uses sparse_matrix from _awesome_cossim_top and vector of unique column values from df.
+        
+        Outputs a Pandas DataFrame of matches and their similarity percentage as a float.
+        """
         non_zeros = sparse_matrix.nonzero()
     
         sparserows = non_zeros[0]
@@ -109,22 +179,48 @@ class DataCleaner:
 
 
     def _replace_matches_df(self):
+        """
+        Replaces matches using all the other DataCleaner methods.
+        Using unique column values, TfidVectorizer uses _ngrams to produce a Tf-idf-weighted document-term matrix.
+        _awesome_cossim_top then finds the similarity between column values, returning a df, then all exact matches are removed.
+        Finally replacing the original values of the df.
+        """
         unique_names = self.df[self.column_name].unique()
-        #print(len(unique_names))
+        #print(len(unique_names))  # Uncomment to compare how many values have been combined
         vectorizer = TfidfVectorizer(min_df=1, analyzer=self._ngrams)
         tf_idf_matrix = vectorizer.fit_transform(unique_names)
-        matches = self._awesome_cossim_top(tf_idf_matrix, tf_idf_matrix.transpose(), 10)
+        matches = self._awesome_cossim_top(tf_idf_matrix, tf_idf_matrix.transpose(), 100)
         matches_df = self._get_matches_df(matches, unique_names)
         matches_df = matches_df[matches_df['similairity'] < 0.99999] # Remove all exact matches
+        # Future improvement: Use the highest value count of either left or right side to determine which to use as final value.
         for left_side, right_side in zip(matches_df['left_side'], matches_df['right_side']):
             for position, column in zip(self.df.index, self.df[self.column_name]):
                 if column == right_side:
                     self.df.at[position, self.column_name] = left_side
-        #print(len(self.df[self.column_name].unique()))
+        #print(len(self.df[self.column_name].unique())) # Uncomment to compare how many values have been combined
         return self.df
 
 
 def build_classes(path: str, lowest_similarity: float, ngram_size: int):
+    """
+    Takes a csv file and combines similar values using ngram_size to determine string chunk sizing.
+    Use return_df_as_csv(build_classes(path: str, lowest_similarity: float, ngram_size: int), filename: str)
+        to output a csv file.
+
+    Parameters:
+        path: str
+            Path to the CSV file to be cleaned
+        lowest_similarity: float
+            Lowest similarity percentage of column values to combine.
+                i.e. 0.9 = words with 90% or more similarity are combined.
+        ngram_size: int
+            Size of string chunks used to assess similarity between two values.
+            3 is normally best but values between 2 and 5 can work.
+
+    Returns:
+        A Pandas Dataframe with all columns' values with similarity at or above lowest_similarity combined.
+
+    """
     df = _csv_to_df(path)
     column_list = _get_df_columns(df)
     count = 0
