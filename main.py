f8912b314e79264ada371adee48075b9319efd35
BOsterbuhr <brodyosterbuhr@gmail.com>
2021-09-27 00:44:52 -0500

    Learning how to intergrate Google Places API

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -39,16 +39,31 @@ def display_landing(request: Request):
 @app.get('/generic', response_class=HTMLResponse)
 def display_generic(request: Request):
     """
-    Displays the index page
+    Displays the generic page
     """
     return templates.TemplateResponse('generic.html', {"request": request})
 
 @app.get('/elements', response_class=HTMLResponse)
 def display_elements(request: Request):
     """
-    Displays the index page
+    Displays the elements page
     """
     return templates.TemplateResponse('elements.html', {"request": request})
+
+@app.get('/map', response_class=HTMLResponse)
+def display_elements(request: Request):
+    """
+    Displays the map page
+    """
+    return templates.TemplateResponse('map.html', {"request": request})
+
+@app.post('/generic')
+def display_map_results(request: Request, location: str=Form(...), locality: str=Form(...), administrative_area_level_1: str=Form(...), postal_code: str=Form(...), country: str=Form(...)):
+    """
+    Displays the generic page with map results
+    """
+    
+    return templates.TemplateResponse('generic.html', {"request": request, "location": location, "locality": locality, "state": administrative_area_level_1, "postal_code": postal_code, "country": country})
 
 
 
